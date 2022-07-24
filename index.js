776793d0d52973bd3a17dfa98990e2d89bff9d9e
grnisan <mdnisanahmed63@gmail.com>
2022-07-25 01:41:35 +0600

    heroku install done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function run() {
 
     try {
         await client.connect();
-        console.log("database connected");
+        console.log("database connected wow!!!");
 
 
     } finally {
