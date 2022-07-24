41839bf953dd3fe2ff094631fce9079ceda0fc79
ShimulChandraDas <msshimul99@gmail.com>
2022-07-25 00:32:16 +0600

    server structure ready

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000
+
+
+app.use(cors());
+app.use(express.json());
+
+//Username:poysha_pay
+//password:ZsRHFYCpIVJa4UrI
+
+
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.abru5.mongodb.net:27017,cluster0-shard-00-01.abru5.mongodb.net:27017,cluster0-shard-00-02.abru5.mongodb.net:27017/?ssl=true&replicaSet=atlas-ybe1bj-shard-0&authSource=admin&retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+
+    try {
+        await client.connect();
+        console.log("database connected");
+
+
+    } finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello BoroLoks!')
+})
+
+app.listen(port, () => {
+    console.log(`Poysha Pay App is ready to transaction on port ${port}`)
+})
