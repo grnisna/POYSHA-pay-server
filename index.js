ae0896c93720bcab5960e999def105081de463cf
grnisan <mdnisanahmed63@gmail.com>
2022-08-05 01:53:02 +0600

    jsonWebToken function added

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ app.use(express.json());
 //Username:poysha_pay
 //password:ZsRHFYCpIVJa4UrI
 
+// //---------varify token --------
+// function varifyToken(req, res, next) {
+//     const getToken = req.headers.authorization;
+//     console.log(getToken);
+//     if (!getToken) {
+//         return res.status(401).send({ message: 'UnAuthorized' });
+//     }
+//     const token = getToken.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+//         if (err) {
+//             return res.status(403).send({ message: 'Forbidden' });
+//         }
+//         else {
+//             req.decoded = decoded;
+//             next();
+//         }
+//     })
+// }
+
 
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.abru5.mongodb.net:27017,cluster0-shard-00-01.abru5.mongodb.net:27017,cluster0-shard-00-02.abru5.mongodb.net:27017/?ssl=true&replicaSet=atlas-ybe1bj-shard-0&authSource=admin&retryWrites=true&w=majority`;
 console.log(uri);
@@ -23,6 +42,16 @@ async function run() {
 
     try {
         await client.connect();
+
+        //========== AUTHENTICATION =======================
+        app.post('/login', async(req,res)=>{
+            const user = req.body;
+            const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN,{
+                expiresIn:'1d'
+            });
+            res.send(accessToken);
+            
+        })
         console.log("database connected wow!!!");
 
 
